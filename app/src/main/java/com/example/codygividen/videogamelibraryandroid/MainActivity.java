f22543e3b3160c5e5cb3ea12abae3a5ac15acd83
CodyGividen <codygividen5@gmail.com>
2018-04-19 16:30:02 -0400

    finished

--- app/src/main/java/com/example/codygividen/videogamelibraryandroid/MainActivity.java
+++ app/src/main/java/com/example/codygividen/videogamelibraryandroid/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.codygividen.videogamelibraryandroid;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -56,6 +60,80 @@ public class MainActivity extends AppCompatActivity implements VideoGameAdapter.
         getSupportFragmentManager().beginTransaction().remove(addGameFragment).commit();
         videoGameAdapter.updateList(videoGameDatabase.videoGameDao().getVideoGames());
         }
+        @Override
+    public Context getContext(){
+        return getApplicationContext();
 
+    }
+
+    @Override
+    public void rowClicked(VideoGame videoGame) {
+       if( videoGame.isCheckedOut()) {
+           checkGameBackIn(videoGame);
+       }else{
+           checkGameOut(videoGame);
+       }
+    }
+
+    @Override
+    public void rowLongClicked(final VideoGame videoGame) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete Game?").setMessage("Are you sure you would like to delete this game?").setNegativeButton(R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                videoGameDatabase.videoGameDao().deleteVideoGame(videoGame);
+
+                videoGameAdapter.updateList(videoGameDatabase.videoGameDao().getVideoGames());
+
+                Toast.makeText(MainActivity.this, "Game has been deleted!", Toast.LENGTH_LONG).show();
+            }
+        }).setPositiveButton(R.string.no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).setIcon(android.R.drawable.ic_dialog_alert).show();
+    }
+
+    private void checkGameOut(final VideoGame videoGame) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("check-out Game?").setMessage("Are you sure you want to check this game out?")
+                .setNegativeButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        videoGame.setCheckedOut(true);
+                        videoGameDatabase.videoGameDao().updateVideoGame(videoGame);
+                        videoGameAdapter.updateList(videoGameDatabase.videoGameDao().getVideoGames());
+                        Toast.makeText(MainActivity.this, "Game is checked out!", Toast.LENGTH_LONG).show();
+
+                    }
+                }).setPositiveButton(R.string.no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).setIcon(android.R.drawable.ic_dialog_alert).show();
+    }
+
+    private void checkGameBackIn(final VideoGame videoGame) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Check-in Game?").setMessage("Are you sure you would like to check in this game?")
+                .setNegativeButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        videoGame.setCheckedOut(false);
+                        //update database with updated game info
+                        videoGameDatabase.videoGameDao().updateVideoGame(videoGame);
+                        //let our adapter know the info in the database has changed to update our view accordingly
+                        videoGameAdapter.updateList(videoGameDatabase.videoGameDao().getVideoGames());
+                        Toast.makeText(MainActivity.this, "Game is checked in!", Toast.LENGTH_LONG).show();
+                    }
+                }).setPositiveButton(R.string.no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).setIcon(android.R.drawable.ic_dialog_alert).show();
     }
 }
--- app/src/main/java/com/example/codygividen/videogamelibraryandroid/VideoGameAdapter.java
+++ app/src/main/java/com/example/codygividen/videogamelibraryandroid/VideoGameAdapter.java
@@ -1,7 +1,9 @@
 package com.example.codygividen.videogamelibraryandroid;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
 import android.view.LayoutInflater;
@@ -10,7 +12,11 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,8 +25,8 @@ public class VideoGameAdapter extends RecyclerView.Adapter<VideoGameAdapter.View
 
     private List<VideoGame> videoGameList;
     private AdapterCallback adapterCallback;
-    @BindView(R.id.item_row_layout)
-    protected Layout layoutcolor;
+//    @BindView(R.id.item_row_layout)
+//    protected Layout layoutcolor;
 
     public VideoGameAdapter(List<VideoGame> videoGameList, AdapterCallback adapterCallback) {
         this.videoGameList = videoGameList;
@@ -36,10 +42,10 @@ public class VideoGameAdapter extends RecyclerView.Adapter<VideoGameAdapter.View
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.bind(position);
+        holder.bind(videoGameList.get(position));
 
-        holder.itemView.setOnClickListener(holder.onClick(videoGameList.get(position));)
-        holder.itemView.setOnLongClickListener();
+        holder.itemView.setOnClickListener(holder.onClick(videoGameList.get(position)));
+        holder.itemView.setOnLongClickListener(holder.onLongClick(videoGameList.get(position)));
     }
 
     @Override
@@ -50,50 +56,70 @@ public class VideoGameAdapter extends RecyclerView.Adapter<VideoGameAdapter.View
         videoGameList = list;
         notifyDataSetChanged();
     }
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.item_row_layout)
-        protected RelativeLayout rowLayout;
+        protected ConstraintLayout rowLayout;
         @BindView(R.id.item_title)
         protected TextView gameTitle;
         @BindView(R.id.item_genre)
         protected TextView gameGenre;
         @BindView(R.id.item_date)
-        private TextView gameDate;
+        protected TextView gameDate;
 
         public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
-            itemView.setOnClickListener(this);
         }
 
 
         public void bind(VideoGame videoGame) {
-            VideoGameApplication videoGameApplication = new VideoGameApplication();
-            Resources res = videoGameApplication
             gameTitle.setText(videoGame.getGameTitle());
-            gameGenre.setText(getReasources().getString(R.string.game_genre, videoGame.));
-            gameDate.setText(videoGameList.get().getDate().toString());
+            gameGenre.setText(adapterCallback.getContext().getString(R.string.game_genre, videoGame.getGameGenre()));
+
+            if (videoGame.isCheckedOut()) {
+                //make the due date visible
+                gameDate.setVisibility(View.VISIBLE);
+                //show the day the game was checked out on
+                videoGame.setDate(new Date());
+                //change background to red
+                rowLayout.setBackgroundResource(R.color.red);
+                //calculate check back in date
+                int numberOfDays = 14;
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(videoGame.getDate());
+                calendar.add(Calendar.DAY_OF_YEAR, numberOfDays);
+                Date date = calendar.getTime();
+                SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy", Locale.US);
+                gameDate.setText(adapterCallback.getContext().getString(R.string.game_due_date, formatter.format(date)));
+            } else {
+                gameDate.setVisibility(View.INVISIBLE);
+                rowLayout.setBackgroundResource(R.color.green);
+            }
         }
 
-        @Override
-        public void onClick(View v) {
-            //To Check out game
-            videoGameList.get(getAdapterPosition()).setGameTitle("CheckedOutGame");
-            notifyDataSetChanged();
-            layoutcolor.
+        public View.OnClickListener onClick(final VideoGame videoGame){
+            return new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    adapterCallback.rowClicked(videoGame);
+                }
+            };
         }
-        //To delete game
-        @Override
-        public boolean onLongClick(View v) {
-
-            return true;
+            //Delete video game
+        public View.OnLongClickListener onLongClick(final VideoGame videoGame) {
+            return new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    adapterCallback.rowLongClicked(videoGame);
+                    return true;
+                }
+            };
         }
-
-
-
-
     }
     public interface AdapterCallback{
-
+        Context getContext();
+        void rowClicked(VideoGame videoGame);
+        void rowLongClicked(VideoGame videoGame);
     }
+
 }
